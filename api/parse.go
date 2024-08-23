53240af00509e4d3be0439536622d7122f400d9b
yi_t <marvelousme@163.com>
2024-08-23 14:32:21 +0800

    feat: vercel

---
+++ api/parse.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wujunwei928/parse-video/handler"
+)
+
+//go:embed templates/*
+var files embed.FS
+var router *gin.Engine
+
+func init() {
+	router = gin.Default()
+	sub, err := fs.Sub(files, "templates")
+	if err != nil {
+		panic(err)
+	}
+	handler.RegisterHandler(router, sub)
+}
+
+// vercel Function
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
+}
